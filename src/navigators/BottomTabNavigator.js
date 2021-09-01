5f8f0ff6ffe3c3d742bee82249be573ee3e4b45d
brianf.baez8@gmail.com <brianf.baez8@gmail.com>
2021-08-31 22:51:31 -0400

    fixed navigators initialRouteName

--- src/navigators/BottomTabNavigator.js
+++ src/navigators/BottomTabNavigator.js
@@ -13,7 +13,7 @@ const Tab = createBottomTabNavigator();
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
-      initialRouteName="Settings"
+      initialRouteName="Home"
       screenOptions={{
         tabBarActiveTintColor: YELLOW,
         tabBarStyle: {
